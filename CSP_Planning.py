357880e450a7a02e1fa59a24bde2eeb886be92be
XuehaoZang <zachary.xuehao.zang@gmail.com>
2026-06-20 23:06:04 -0400

    chores: fixed minor bugs

--- CSP_Planning.py
+++ CSP_Planning.py
@@ -3,18 +3,20 @@ import json
 from utils.viz import print_bay
 from utils.dataset import total_containers, remaining_PODs
 
-# 装入箱子, TODO 从origin 0 开始放
-def CSP(bay, left_containers):
+# 装入箱子, TODO 从 current port = 0 开始放, current port 逐步增加
+def CSP(init, current_port, left_containers):
+    # 这里应该套一个大循环POL，相当于模拟遍历各个港，还要加上discharge的逻辑
+    
     num_left_containers = total_containers(left_containers)
     if num_left_containers == 0:
         return True # 安全放完, 没有dead slot
     # 1. 根据现在的bay, 计算每个位置的候选(是一个集合比如{1,2,3})
-    current_candidates = cal_candidates(bay)
+    current_candidates = cal_candidates(init)
     # 缩小搜索空间 - 空位 且 候选与剩余箱子有交集
     avail_PODs = remaining_PODs(left_containers)
     choices = {}
-    for idx in np.ndindex(bay.shape):
-        if bay[idx] != -1:
+    for idx in np.ndindex(init.shape):
+        if init[idx] != -1:
             continue
         valid = current_candidates[idx] & avail_PODs
         if not valid:
@@ -28,28 +30,30 @@ def CSP(bay, left_containers):
     
     # MRV: 选候选最少的位置
     pos = min(choices, key=lambda p: len(choices[p]))
-
+    
     for POD in sorted(choices[pos]): # 遍历这个位置所有可放的目的港, 比如能放{1,2}, 依次试
-        # 比如现在在试dest = 0, 从没放的箱子里找一个dest = 0的放入
+        # 当前港口是固定的，只从 current_POL 的箱子里选
+        if left_containers.get(current_POL, {}).get(POD, 0) == 0:
+            continue  # 当前港口没有去 选定POD 的箱子，跳过
 
-        # TODO 每次只能考虑当前origin, 这里逻辑要改, 只能从当前origin选
-        POL = None
-        for o, ds in left_containers.items():
-            if ds.get(POD, 0) > 0:       # 这个 origin 还有去 dest 的箱子, default_value=0
-                POL = o
-                break      
-        if POL is None:
-            continue
+        # # TODO 每次只能考虑当前origin, 这里逻辑要改, 只能从当前origin选
+        # POL = None
+        # for o, ds in left_containers.items():
+        #     if ds.get(POD, 0) > 0:       # 这个 origin 还有去 dest 的箱子, default_value=0
+        #         POL = o
+        #         break      
+        # if POL is None:
+        #     continue
 
         # 放箱子
-        bay[pos] = POD
+        init[pos] = POD
         left_containers[POL][POD] -= 1
 
-        if CSP(bay, left_containers):
+        if CSP(init, current_port, left_containers):
             return True
 
         # 回溯
-        bay[pos] = -1
+        init[pos] = -1
         left_containers[POL][POD] += 1
 
 def cal_candidates(bay):
